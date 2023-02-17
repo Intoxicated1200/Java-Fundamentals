a492af09cd6a511b6623e919ec76a899b3e4a08d
cagold022 <cagold022@student.wethinkcode.co.za>
2023-02-17 11:41:58 +0200

    first

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 public class Main{
 
     public static void main(String[] args){
-        Print();
+        String[] list = {"Hello", "I", "am", "a", "list"};
+        String word = "Hello, I am a String";
+        print(word);
+        forLoopList(list);
+        forLoopWord(word);
 }
 
-    public static void Print(){
-        System.out.println("Hello, World!");
+    public static void print(String word){
+        System.out.println(word);
+    }
+
+    public static void forLoopList(String[] list){
+        for(String word:list){
+            System.out.println(word);
+        }
+    }
+
+    public static void forLoopWord(String word){
+        for(int index = 0; index < word.length(); index++){
+            System.out.println(word.charAt(index));
+        }
     }
 }
