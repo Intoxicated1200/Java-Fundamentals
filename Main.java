e016810bf8f989dcff6a6471c50797e5ba795535
kyle <kylebmoodley@gmail.com>
2023-02-19 17:10:02 +0200

    my shiz

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ public class Main{
         print(word);
         forLoopList(list);
         forLoopWord(word);
-}
+        third_decision(1);}
+
 
     public static void print(String word){
         System.out.println(word);
@@ -23,10 +24,29 @@ public class Main{
             System.out.println(word.charAt(index));
         }
     }
-    
-}
+
+    private static void third_decision(int num) {
+        int my_num = 15;
+        if (num>my_num){
+            System.out.println("your number is strong");
+        }else if (my_num == num){
+            System.out.println("our numbers is equal to rand");
+        }else{
+            System.out.println("my number is stronger");
+        }
+
+    private static void second_decision(int numb) {
+        int a_num = 29 ;
+        if (numb>a_num){
+            System.out.println("your number is strong");
+        }else{
+            System.out.println("dollar is stronger");
+        }
+    }
+}}
 
 //1. Data types, the importance of them
 //2. Hello, world
 //3. Variable Declaring
-//4. 
+//4. if statements
+//5. loops
